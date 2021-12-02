b0ec43aba6ddbd0225f61db279d70a41f4a6b92a
Julian Lastra <Julian.Lastra@gds.ey.com>
2021-12-02 04:26:11 -0300

    Patron Observer Hello world

---
+++ Press/Observer/Subject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+    public class Subject : ISubject
+    {
+        public int State { get; set; } = -0;
+
+        private List<IObserver> ListObservers = new();
+
+        public void Attach(IObserver observer)
+        {
+            ListObservers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            ListObservers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in ListObservers)
+            {
+                observer.Update(this);
+            }
+        }
+
+        public void logic()
+        { 
+        
+        }
+    }
+}
